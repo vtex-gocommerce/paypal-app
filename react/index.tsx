5c9bb96584a2b11429101e2e86cfb13bb0768d09
Augusto Lazaro <aemlazaro@gmail.com>
2020-03-27 17:45:20 -0300

    Apply custom setup route

--- react/index.tsx
+++ react/index.tsx
@@ -11,7 +11,7 @@ interface PaymentFormState {}
 class PaymentFormComponent extends React.PureComponent<PaymentFormProps, PaymentFormState> {
   render() {
     const { intl } = this.props
-    const intlPrefix = 'admin.payment.paypal'
+    const intlPrefix = 'admin/payment.paypal'
     const optionsInstallments = new Array(12).fill(0).map(function(_, i) {
       const curr = i + 1
       return {
